16fabb24ff0307e56af22a5fe6037d486086295e
Rajat Vij <rajatvij@gwdev-rajatvij-14.wrlc.org>
2015-08-28 14:23:42 -0400

    Updated the directory layout of requirements and setting files

--- sfm/sfm/settings/common.py
+++ sfm/sfm/settings/common.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sfm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sfmdatabase',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
