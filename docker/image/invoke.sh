52a31f4d25fef52b0c0e7e1e85f21f3fa13a6053
Rajat Vij <rajatvij@gwdev-rajatvij-14.wrlc.org>
2015-09-02 11:13:31 -0400

    Added docker files

---
+++ docker/image/invoke.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo "Waiting for db"
+python /opt/sfm-setup/appdeps.py --wait-secs 30 --port-wait db:5432 --file /opt/sfm-ui
+if [ "$?" = "1" ]; then
+    echo "Problem with application dependencies."
+    exit 1
+fi
+
+echo "Updating requirements"
+pip install -r /opt/sfm-ui/requirements/requirements.txt
+
+echo "Writing local_settings"
+echo "env={}" > /opt/sfm-ui/sfm/sfm/settings/common.py
+env | grep 'SFM_\|\DB_' | sed 's/\(.*\)=\(.*\)/env["\1"]="\2"/' >> /opt/sfm-ui/sfm/sfm/settings/common.py
+cat /tmp/common.py >> /opt/sfm-ui/sfm/sfm/settings/common.py
+
+echo "Copying config"
+cp /tmp/wsgi.py /opt/sfm-ui/sfm/sfm/
+
+echo "Syncing db"
+/opt/sfm-ui/sfm/manage.py syncdb --noinput
+
+echo "Migrating db"
+/opt/sfm-ui/sfm/manage.py migrate --noinput
+
+echo "Running server"
+#Not entirely sure why this is necessary, but it works.
+/etc/init.d/apache2 start
+#Make sure apache has started
+/etc/init.d/apache2 status
+while [ "$?" != "0" ];  do
+    echo "Waiting for start"
+    sleep 1
+    /etc/init.d/apache2 status
+done
+echo "Stopping server"
+/etc/init.d/apache2 graceful-stop
+#Make sure apache has stopped
+/etc/init.d/apache2 status
+while [ "$?" = "0" ];  do
+    echo "Waiting for stop"
+    sleep 1
+    /etc/init.d/apache2 status
+done
+echo "Starting server again"
+apachectl -DFOREGROUND
