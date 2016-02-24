a6c2673bc1ae28cfd680b482669dc320e5ffa11a
Justin Littman <justinlittman@gwu.edu>
2016-02-24 14:57:24 -0500

    Bumped version and updated sfm-utils version.

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u'The George Washington University Libraries'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.3.1'
+release = '0.4.0'
 # The short X.Y version.
 version = release[0:release.rindex(".")]
 
