5c3755804c8879e95da8491fad802f96ef5ee297
Justin Littman <justinlittman@gwu.edu>
2016-11-15 12:36:57 -0500

    refs #532. Disables scheduled serializations.

--- sfm/sfm/settings/common.py
+++ sfm/sfm/settings/common.py
@@ -201,7 +201,9 @@ DATA_THRESHOLD = env.get('DATA_VOLUME_THRESHOLD', '10GB')
 # sfm processing space threshold to send notification email,only ends with MB,GB,TB. eg. 500MB,10GB,1TB
 PROCESSING_THRESHOLD = env.get('PROCESSING_VOLUME_THRESHOLD', '10GB')
 
-PERFORM_SERIALIZE = env.get('SFM_PERFORM_SERIALIZE', 'True') == 'True'
+# Temporarily disabling scheduled serialization due to https://github.com/gwu-libraries/sfm-ui/issues/532.
+# PERFORM_SERIALIZE = env.get('SFM_PERFORM_SERIALIZE', 'True') == 'True'
+PERFORM_SERIALIZE = False
 SERIALIZE_HOUR = env.get('SFM_SERIALIZE_HOUR', '3')
 SERIALIZE_MINUTE = env.get('SFM_SERIALIZE_MINUTE', '0')
 
