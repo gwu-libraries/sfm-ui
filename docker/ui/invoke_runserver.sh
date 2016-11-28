ed69d1290c4af9586dc8355a7d7ad7c81bdbe0c0
Justin Littman <justinlittman@gwu.edu>
2016-11-27 20:52:38 -0500

    refs #532. Changes to use sfm user and group. Turns scheduled serialization back on. Turns off lazy initialization of sfm app when running under Apache.

--- docker/ui/invoke_runserver.sh
+++ docker/ui/invoke_runserver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Starting message consumer"
-/opt/sfm-ui/sfm/manage.py startconsumer &
+gosu sfm /opt/sfm-ui/sfm/manage.py startconsumer &
 
 echo "Running server"
 export SFM_RUN_SCHEDULER=True
-/opt/sfm-ui/sfm/manage.py runserver 0.0.0.0:80
+gosu sfm /opt/sfm-ui/sfm/manage.py runserver 0.0.0.0:8080
