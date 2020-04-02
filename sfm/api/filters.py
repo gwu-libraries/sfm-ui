57b1a04d954e880bd96d5cb16b5f6780370e7687
Laura Wrubel <lwrubel@gwu.edu>
2020-04-02 16:50:41 -0400

    api fixes with django-filter upgrade

--- sfm/api/filters.py
+++ sfm/api/filters.py
@@ -1,19 +1,16 @@
-#from django_filters import FilterSet, CharFilter, IsoDateTimeFilter
 from django_filters import rest_framework as filters
 from ui.models import Warc, Collection
-#from django_filters import Filter
-from django_filters.fields import Lookup
+from django_filters.widgets import CSVWidget
 
 
-class ListFilter(filters.Filter):
-    def filter(self, qs, value):
-        return super(ListFilter, self).filter(qs, Lookup(value.split(u","), "in"))
+class ListFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
 
 
 class WarcFilter(filters.FilterSet):
     # Allows queries like /api/v1/warcs/?collection=39c00280274a4db0b1cb5bfa4d527a1e
     collection = filters.CharFilter(field_name="harvest__historical_collection__collection_id")
-    seed = ListFilter(field_name="harvest__historical_seeds__seed_id", distinct=True)
+    seed = ListFilter(widget=CSVWidget, field_name="harvest__historical_seeds__seed_id", distinct=True, lookup_expr='in') 
     harvest_date_start = filters.IsoDateTimeFilter(field_name="harvest__date_started", lookup_expr='gte')
     harvest_date_end = filters.IsoDateTimeFilter(field_name="harvest__date_started", lookup_expr='lte')
     created_date_start = filters.IsoDateTimeFilter(field_name="date_created", lookup_expr='gte')
--- sfm/api/urls.py
+++ sfm/api/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import WarcViewSet, CollectionViewSet
 
 router = DefaultRouter()
-router.register(r'warcs', WarcViewSet, base_name="warc")
-router.register(r'collections', CollectionViewSet, base_name="collection")
+router.register(r'warcs', WarcViewSet, basename="warc")
+router.register(r'collections', CollectionViewSet, basename="collection")
 urlpatterns = router.urls
