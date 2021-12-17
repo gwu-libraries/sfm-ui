abbb33c6b35041e81e40051555594129f67f4676
Laura Wrubel <lwrubel@gwu.edu>
2021-12-17 14:34:58 -0500

    Preliminary work on twarc2 REST API harvests.

--- sfm/ui/test_forms.py
+++ sfm/ui/test_forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group
 from django.test import TestCase, RequestFactory
 
 from .forms import CollectionSetForm, ExportForm, CredentialWeiboForm, CredentialFlickrForm, \
-    CredentialTwitterForm, CollectionTwitterSearchForm, SeedTwitterUserTimelineForm
+    CredentialTwitterForm, CredentialTwitter2Form, CollectionTwitterSearchForm, SeedTwitterUserTimelineForm
 from .views import CollectionSetUpdateView
 from .models import User, CollectionSet, Credential, Collection, Seed
 
@@ -210,6 +210,35 @@ class CredentialTwitterFormTest(TestCase):
                                                '"access_token": "dummy_access_token",'
                                                '"access_token_secret": "dummy_access_token_secret"}')
 
+class CredentialTwitter2FormTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.path = "/ui/credentials/twitter2/create/"
+        self.user = User.objects.create_superuser(username="test_user",
+                                                  email="test_user@test.com",
+                                                  password="test_password")
+        self.data = {
+            "name": "test_twitter2_credential",
+            "user": self.user.pk,
+            "platform": "twitter2",
+            "consumer_key": "dummy_consumer_key",
+            "consumer_secret": "dummy_consumer_secret",
+            "access_token": "dummy_access_token",
+            "access_token_secret": "dummy_access_token_secret",
+            "bearer_token": "dummy_bearer_token",
+            "date_added": "12/17/2021"
+        }
+
+    def test_form(self):
+        form = CredentialTwitter2Form(self.data)
+        form.instance.user = self.user
+        self.assertTrue(form.is_valid())
+        credential = form.save()
+        self.assertJSONEqual(credential.token, '{"consumer_key": "dummy_consumer_key",'
+                                               '"consumer_secret": "dummy_consumer_secret",'
+                                               '"access_token": "dummy_access_token",'
+                                               '"access_token_secret": "dummy_access_token_secret",'
+                                               '"bearer_token":"dummy_bearer_token"}')
 
 class CredentialWeiboFormTest(TestCase):
     def setUp(self):
