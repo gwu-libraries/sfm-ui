a517dae837d0d41eb0e39f642988f45af23a8bf7
Adhithya Kiran <adhithyakiran@gwdev-kiran.wrlc.org>
2022-08-11 15:13:15 -0400

    UI Changes

--- sfm/ui/templates/ui/date_warning_snippet.js
+++ sfm/ui/templates/ui/date_warning_snippet.js
@@ -6,7 +6,7 @@
 
 
         var warning_div2 = $("#date_warning");
-        warning_div2.htmldate_warning("Use start and end times in order to avoid using up monthly limit imposed by the API.See Twitter's <a href='https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api#v2-access-level' target='_blank'>API access levels and versions here</a>.");
+        warning_div2.html("Use start and end times in order to avoid using up monthly limit imposed by the API.See Twitter's <a href='https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api#v2-access-level' target='_blank'>API access levels and versions here</a>.");
         warning_div2.removeClass();
         warning_div2.css('position', 'absolute');
         warning_div2.css('top', 175); 
