e0d4dca518d35f7a914ff57104f8c906faefe1f3
Dolsy Smith <dsmith@gwu.edu>
2023-04-11 14:58:35 -0400

    Fixed button permissions

--- sfm/ui/views.py
+++ sfm/ui/views.py
@@ -275,8 +275,9 @@ class CollectionDetailView(LoginRequiredMixin, CollectionSetOrCollectionVisibili
         context["can_edit"] = can_edit
         # Seed add buttons should be enabled.
         context["can_edit_seeds"] = can_edit if self.object.is_streaming() else self.object.is_active and has_perms
+        context["can_toggle_on"] = has_perms 
         # Don't allow turning on Twitter collections not in settings list of allowed types
-        context["can_toggle_on"] = has_perms and ((not self.object.harvest_type.startswith('twitter')) or (self.object.harvest_type in settings.TWITTER_COLLECTION_TYPES))
+        context["disabled_collection_type"] = self.object.harvest_type.startswith('twitter') and (self.object.harvest_type in settings.TWITTER_COLLECTION_TYPES)
         context["can_toggle_active"] = not self.object.is_on and has_perms
         # If last harvest is stopping
         context["stream_stopping"] = self.object.last_harvest().status == Harvest.STOP_REQUESTED \
